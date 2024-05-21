68330841390293d3ff6d6f9e3f89e1ec923f5a8d
MargoF94 <greenmonochrome@gmail.com>
2024-05-21 19:10:07 +0900

    second commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import SideBar from "@/components/ui/SideBar";
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: 'Dude', lastName: 'JSM' }
+
     return (
-        <main>
-            SIDEBAR
+        <main className="flex h-screen w-full font-inter">
+            <SideBar user={loggedIn} />
             {children}
         </main>
     );
