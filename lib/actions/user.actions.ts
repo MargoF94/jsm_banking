fa4956524c352f3575679f52eb0792a888a93309
MargoF94 <greenmonochrome@gmail.com>
2024-05-26 17:22:35 +0900

    20240526

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,54 @@
+'use server'
+
+import { ID } from "node-appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
+import { cookies } from "next/headers"
+import { parseStringify } from "../utils"
+
+export const signIn = async () => {
+  try {
+
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
+
+export const sigUpn = async (userData: SignUpParams) => {
+  const {email, password, firstName, lastName} = userData
+
+  try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    // cannot pass large objects like user object through server actions
+    return parseStringify(newUser)
+  } catch (error) {
+    console.log('Error', error)
+  }
+}
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
